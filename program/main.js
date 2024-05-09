cf00d4082f06d31ed39a8058facf15a5e0b44c30
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-05-09 14:11:36 -0500

    Added initial settings

--- program/main.js
+++ program/main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, ipcMain, systemPreferences, desktopCapturer } = require('electron')
-const IS_OSX = process.platform === "darwin";
+const {app, BrowserWindow, ipcMain, systemPreferences, desktopCapturer} = require('electron');
+const { openSystemPreferences } = require("electron-util");
+const IS_OSX = process.platform === 'darwin';
 
 const createWindow  = () => {
     win = new BrowserWindow({
