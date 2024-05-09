59e578e11daf31bbee9b82f39c263f8a345527e2
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-05-09 11:45:40 -0500

    InitialFiles

--- program/main.js
+++ program/main.js
@@ -1,13 +1,35 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, systemPreferences, desktopCapturer } = require('electron')
+const IS_OSX = process.platform === "darwin";
 
 const createWindow  = () => {
     win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            devTools: true,
+            sandbox: false
+        }
     })
 
     win.loadFile('index.html')
 }
+
+const mainMenu = [
+    {
+        label: 'Dev',
+        submenu: [
+            {
+                role: 'toggledevtools'
+            },
+            {
+                role: 'reload'
+            },
+            {
+                role: 'forcereload'
+            }
+        ]
+    }
+]
 
 app.on("ready", () => {
     createWindow()
