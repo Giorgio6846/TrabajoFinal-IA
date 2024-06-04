067e05e44c51dfb49554be757166a0ad3dc2fdd1
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-06-03 20:45:50 -0500

    Added server conncetion support

--- program/main.js
+++ program/main.js
@@ -3,16 +3,18 @@ const { app, BrowserWindow, ipcMain, Menu } = require("electron");
 const path = require("path");
 const screenshot = require("screenshot-desktop");
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true, // Ensure context isolation is enabled
       nodeIntegration: false, // Disable node integration for security
+      devTools: true,
+      sandbox: false
     },
-  });
+  })
 
   mainWindow.loadFile("./src/pages/index.html");
 }
@@ -38,7 +40,8 @@ const mainMenu = [
             {
                 label: 'Server Connection',
                 click: async() => {
-                    win.loadFile(path.join(__dirname,'src','views','serverConnection.html'))
+                    mainWindow.loadFile("./src/pages/serverConnection.html")
+                    console.log("TEST")
                 }
             }
         ]
@@ -47,6 +50,7 @@ const mainMenu = [
 
 const menu = Menu.buildFromTemplate(mainMenu)
 Menu.setApplicationMenu(menu)
+
 app.whenReady().then(() => {
   createWindow();
 
