202811d0cd2e8047e53d509b523e4bfed31c04e6
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-06-07 22:29:41 -0500

    Changed support for workers

--- dataset/workspaceGame/WorkerPC.py
+++ dataset/workspaceGame/WorkerPC.py
@@ -1,51 +1,70 @@
-import tensorflow as tf
 import socket
-from multiprocessing import Process, Queue
-import time
-import numpy as np
-
-from Tools import SaveModel
+from multiprocess import Process, Manager
+from time import sleep
 from Environment import BJEnvironment
 from Agent import DQNAgent
 
 VERSION = 2
 COMPLETEDVERSION = 1
+MINIEPISODES = 50
 
 class WorkerPC:
-    def __init__(self):
+    def __init__(self, Num_Process = 4):
         self.Procc = 4
         self.state_size = 3
         self.action_size = 3
         self.batch_size = 32
-        self.models = {}
-    def workers(self):
-        q = [(Queue()) for index in range(self.Procc)]
-        doneArray = np.zeros(self.Procc)
+        self.manager = Manager()
+
+        #Vector of workers
+        self.workersHome = {}
+
+        self.modelCoordinator = 1
+        #Waiting, Working, Finished
+        self.statusWorkers = self.manager.dict()
+        #Dictionary in which the models are stored     
+        self.models = self.manager.dict()
+
+        self.initWorkers()
+
+    def initWorkers(self):
+        for index in range(self.Procc):
+            self.statusWorkers[index] = "Waiting"
+            
+            self.workersHome[index] = Process(target=self.training, args=(index, MINIEPISODES, self.modelCoordinator, self.statusWorkers, self.models))
+            self.workersHome[index].start()
+            
+    #Index: The index of the device
+    #Episodes: Amount of episodes for the process to train
+    #Model: The model to use to train
+    #Status: To insert the status of work
+    #Models: To store the model
+    
+    def training(self, Index, Episodes, Model, Status, Models):        
+        if Status[Index] == "Waiting":
+            Status[Index] = "Working"
+            
+            agent = DQNAgent(self.state_size, self.action_size, 0.01, Episodes, self.batch_size, "./models/v{VERSION}/logs".format(VERSION=VERSION))
+            env = BJEnvironment()
+
+            #agent.model = Model
+            
+            for ep in range(Episodes):
+                print(ep)
+                agent.train(env, False)
+
+            Models[Index] = agent.model
+            Status[Index] = "Finished"
         
-        work = [Process(target=self.test, args=(50,q[index], doneArray, index)) for index in range(self.Procc)]
-        
-        while True:            
-            for worker in work:
-                if not worker.is_alive():
-                   worker.start()
-            time.sleep(1)
+    def factory(self):
+        while True:
             
             for index in range(self.Procc):
-                if doneArray[index] == 1:
-                    print(q[index].get())
+                if self.statusWorkers[index] == "Finished":
+                    print("Process {index} has finished".format(index= index))
+                    print(self.models[index].get_weights())
+            sleep(1)
             
-        
-    def test(self, EPISODES, model, array, index):
-        agent = DQNAgent(self.state_size, self.action_size, 0.01, EPISODES, self.batch_size, "./models/v{VERSION}/logs".format(VERSION=VERSION))
-        env = BJEnvironment()
-        
-        for ep in range(EPISODES):
-            print(ep)
-            agent.train(env, False)
-
-        array[index] = 1
-        model.put({agent.model, 1})
-       
 if __name__ == "__main__": 
-    worker = WorkerPC()
-    worker.workers()
+    worker = WorkerPC(4)
+    worker.factory()
