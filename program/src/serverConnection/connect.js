04fe654ad060aa1f8acc7a00b5848dedb97db698
Mathias <mathiashv2014@gmail.com>
2024-06-10 23:10:04 -0500

    Primera version Server

--- program/src/serverConnection/connect.js
+++ program/src/serverConnection/connect.js
@@ -1,18 +1,31 @@
-var zmq  = require("zeromq");
-sock = new zmq.Request;
-
+var zmq = require("zeromq");
+sock = new zmq.Request();
 const ipAddress = "tcp://127.0.0.1:5555";
-
 const decoder = new TextDecoder();
+const fs = require("fs");
 
-async function run(dataJSON) {
-    sock.connect(ipAddress)
-    console.log("Connected to server in tcp://0.0.0.0:5555")
+const filePath = "../screenshots/screenshot-1.jpg";
 
-    await sock.send(dataJSON)
-    const result = await sock.receive()
-    
-    dataServer = decoder.decode(result[0])
-    console.log(dataServer)
-    return dataServer   
+function imageToBase64(filePath) {
+  const image = fs.readFileSync(filePath);
+  const base64Image = image.toString("base64");
+  return base64Image;
 }
+
+async function run(filePath) {
+  sock.connect(ipAddress);
+  console.log("Connected to server at tcp://127.0.0.1:5555");
+
+  setInterval(async () => {
+    const base64Image = imageToBase64(filePath);
+
+    await sock.send(base64Image);
+    console.log("Image sent to server");
+
+    const result = await sock.receive();
+    const dataServer = decoder.decode(result[0]);
+    console.log("Received from server:", dataServer);
+  }, 1000);
+}
+
+run(filePath);
